1c3b9e219d9ecbde024a045e14774b2220edbf9d
Rehan Adi Purwana <rehanadipurwana@gmail.com>
2025-05-02 11:09:47 +0700

    feat: add lyric mutation

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,36 +1,60 @@
-import React from "react";
-import gql from 'graphql-tag'
-import { graphql } from "react-apollo";
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/fetchSongs';
 
-const SongList = ({ data }) => {
-  const renderSongs = () => {
-    return data.songs.map((song) => {
+class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({
+      variables: { id }
+    })
+    .then(() => this.props.data.refetch());
+  }
+
+  renderSongs() {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          <Link to={`/songs/${id}`}>
+            {title}
+          </Link>
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(id)}
+          >
+            delete
+          </i>
         </li>
       );
     });
-  };
-
-  if (data.loading) {
-    return <div>Loading...</div>;
   }
 
-  return (
-    <ul className="collection">
-      {renderSongs()}
-    </ul>
-  );
-};
+  render() {
+    if (this.props.data.loading) { return <div>Loading...</div>; }
 
-const query = gql`
-  {
-    songs {
+    return (
+      <div>
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
+        <Link
+          to="/songs/new"
+          className="btn-floating btn-large red right"
+        >
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation DeleteSong($id: ID){
+    deleteSong(id: $id) {
       id
-      title
     }
   }
 `;
 
-export default graphql(query)(SongList);
+export default graphql(mutation)(graphql(query)(SongList));
